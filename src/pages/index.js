65258e2210e764ac8b74e42ce6bef05948f49d1d
SigneBennmarker <signe.bennmarker@hotmail.com>
2021-04-07 22:25:24 +0200

    justeringar till coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 
 import '../style.css';
 
+
 import Header from '../components/Header'; 
 import ContentsGrid from '../components/ContentsGrid';
 var ReactDOM = require('react-dom');
@@ -26,12 +27,11 @@ var ReactDOM = require('react-dom');
 
 
 
-
 export default function Home() {
 
     return (
 
-        <div className = "comingSoon">
+        <div className = "comingSoon" onLoad = "FadeIn()">
            
         <h1>COMING SOON</h1>
        
