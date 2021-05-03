af9daec7a6e87766862537f613c83c135d9619a0
SigneBennmarker <signe.bennmarker@hotmail.com>
2021-05-03 14:34:22 +0200

    :)

--- src/pages/mywork.js
+++ src/pages/mywork.js
@@ -1,45 +1,18 @@
-import React, { useRef, useEffect, useContext, createContext } from "react"
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Link,
-  useLocation,
-  Router,
-} from "react-router-dom"
-// import Grid from '@material-ui/core/Grid';
-//¨
+import React from "react"
+
 import "../style.css"
-import ContentsGrid from "../components/ContentsGrid"
+
 import NavBar from "../components/Nav"
-import AboutMe from "../components/AboutMe"
-import Contact from "../components/Contact"
+
 import WorkGrid from "../components/WorkGrid"
-import MiddleSection from'../components/MiddleSection'
-import { Fade, Container } from '@material-ui/core';
-import Tottot from '../components/Tottot'
-import LarsViner from '../components/LarsViner'
-
-// const WorkContext = React.createContext(WorkRef);
-// const ContactContext = React.createContext();
-
 
 export default function MyWork() {
-  // const AboutRef = useRef(null)
-  // const WorkRef = useRef(null);
-  // const ContactRef = useRef(null);
-  //const AboutContext = React.createContext(AboutContext);
-
-
   return (
-    <>
-
-    
-    <NavBar></NavBar>
-    <WorkGrid/>
-
-     </>
-    )
+    <div>
+      <NavBar></NavBar>
+      <WorkGrid />
+    </div>
+  )
 }
 
 // export default function Home() {
